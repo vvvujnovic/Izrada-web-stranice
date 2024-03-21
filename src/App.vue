eb1b40bc0504b6e5d4ae25f81ffedb5f3413dbbe
vvujnovic <162925491+vvujnovic@users.noreply.github.com>
2024-03-21 20:05:29 +0100

    Unos registracije

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/UserLogin">Prijava</router-link>
-    <router-link to="/Signup">Siginup</router-link>
+    <router-link to="/SignUpForm">Registracija</router-link>
   </nav>
   <router-view/>
 </template>
