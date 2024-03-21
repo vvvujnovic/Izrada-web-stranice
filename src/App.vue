1421171b70ed560daaad922554b4cb9554dc3bff
vvujnovic <162925491+vvujnovic@users.noreply.github.com>
2024-03-21 23:52:25 +0100

    Uređivanje navigacijske trake

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
+
 <template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">TRIO RIO OBRT</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto"> 
+          <li class="nav-item">
+            <a class="nav-link" href="#">Prijava</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registracija</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="UserLogin">
+    </div>
+  </div>
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/UserLogin">Prijava</router-link>
@@ -6,6 +30,9 @@
   </nav>
   <router-view/>
 </template>
+
+
+
 
 <style lang="scss">
 #app {
